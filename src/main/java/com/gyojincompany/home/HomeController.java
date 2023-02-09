aed977c008fc48b6e981cc469ed726b060a0c6dd
gyojincompany <luckyred@gmail.com>
2023-02-09 12:53:20 +0900

    @modelAttribute 적용 버전

--- src/main/java/com/gyojincompany/home/HomeController.java
+++ src/main/java/com/gyojincompany/home/HomeController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -47,7 +48,7 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value = "/joinOk")
-	public String joinOk(MemberDto memberDto) {
+	public String joinOk(@ModelAttribute("dto") MemberDto memberDto) {
 		
 		return "joinOk";
 	}
--- src/main/webapp/WEB-INF/views/joinOk.jsp
+++ src/main/webapp/WEB-INF/views/joinOk.jsp
@@ -9,10 +9,10 @@
 <body>
 	<h2>가입된 회원 정보</h2>
 	<hr>
-	아이디 : ${memberDto.mid }<br><br>
-	비밀번호 : ${memberDto.mpw }<br><br>
-	회원이름 : ${memberDto.mname }<br><br>
-	이메일 : ${memberDto.memail }<br><br>
-	전화번호 : ${memberDto.mphone }<br><br>
+	아이디 : ${dto.mid }<br><br>
+	비밀번호 : ${dto.mpw }<br><br>
+	회원이름 : ${dto.mname }<br><br>
+	이메일 : ${dto.memail }<br><br>
+	전화번호 : ${dto.mphone }<br><br>
 </body>
 </html>
